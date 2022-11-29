4aa234b97dfdaf384ce69903fcc532a731d3e5b5
Josemi <josemiguel2201@gmail.com>
2022-11-29 22:56:12 +0100

    Añadir rutas y componentes principales

---
+++ src/routes/ProductDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header } from '../components/Header'
+
+export const ProductDetail = () => {
+  return (
+    <>
+      <Header />
+      <div>ProductDetail</div>
+    </>
+  )
+}
